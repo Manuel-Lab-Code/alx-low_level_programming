3cd86c5896f1e91ed5e4cb6470f90db4cdb110c3
Manuel-Lab-Code <emmanueltobalase@gmail.com>
2022-07-02 13:39:47 -0700

    Size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,20 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-	
-
-	int main(void)
+* main -Entry Point
+* Finding size of various data types
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of a int: %zu byte(s)\n", sizeof(int)); 
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long lomg int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	int a;
+	long long int c;
+	char d;
+	float f;
+ 	long int b;
+ 	      printf("Size of a char: %lu byte(s)\n", sizeof(d));
+	      printf("Size of an int: %lu byte(s)\n", sizeof(a));
+      	      printf("Size of a long int: %lu byte(s)\n", sizeof(b));
+	      printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
+	      printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
-	
 }
