f48fc11f5730386bd362bb8fee2b1adf436e0654
Manuel-Lab-Code <emmanueltobalase@gmail.com>
2022-07-02 13:28:11 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - Entry Point
+ * My hello, printf task
+ * Return: 0 (success)
+ */
 
-	int main(void)
+int main(void)
+
 {
-	printf("Programming is like building a multilingual puzzle, \n");
-	return (0);
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+		return (0);
+
 }
