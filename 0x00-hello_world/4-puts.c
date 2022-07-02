4cca491bd45ad06ce3fff21dfc22c3370515cfe0
Manuel-Lab-Code <emmanueltobalase@gmail.com>
2022-07-02 13:12:48 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+
 /**
-* main - Prints "Programming is like building a multilingual 
-* puzzle, followed by the new line.
-*
-* Return: Always 0 (Success)
-*/
+ *
+ *  *main - Entry point
+ *
+ *   *Return: Always 0 (Success)
+ *
+ *    */
+
 int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}		
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
