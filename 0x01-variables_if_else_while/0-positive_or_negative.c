1977553025287af957c47b9d933f16c90ac4e2d1
Manuel-Lab-Code <emmanueltobalase@gmail.com>
2022-07-03 06:18:33 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,23 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
 /**
-*main - Entry point
-*Return: Always 0(success)
-*/
-
+ * main - Entry point
+ * Description: 'This program will assign a random number to the veriable n'
+ * Return: Always 0 (Succes)
+ */
 int main(void)
-{
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-{
-printf("%i is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%i is zero\n", n);
-}
-else if (n < 0)
-{
-printf("%i is negative\n", n);
-}
-return (0);
+{ 
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+
+	return (0);
 }
